9829f3a2b25705334d41720a82af6c53922a94f1
Induce <aa5619721@gmail.com>
2026-03-11 17:54:08 +0300

    add CompoundingFrequency

---
+++ src/main/java/com/induce/profitability_calculation_service/model/CompoundingFrequency.java
@@ -0,0 +1,15 @@
+package com.induce.profitability_calculation_service.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CompoundingFrequency {
+    MONTHLY(1),
+    QUARTERLY(3),
+    SEMI_ANNUALLY(6),
+    ANNUALLY(12);
+
+    private final int months;
+}
